c120deaae4ac1f98b655a731caee1c7543d47057
phaolapaixao <165348503+phaolapaixao@users.noreply.github.com>
2025-02-15 14:16:12 -0300

    Update neopixel_pio.c

--- neopixel_pio.c
+++ neopixel_pio.c
@@ -91,55 +91,10 @@ int getIndex(int x, int y) {
   }
 }
 
-const uint I2C_SDA = 14;
-const uint I2C_SCL = 15;
-
 int main() {
   const uint BUTTON_A = 5;
   const uint BUTTON_B = 6;
 
-  /*stdio_init_all();   // Inicializa os tipos stdio padrão presentes ligados ao binário
-
-    // Inicialização do i2c
-    i2c_init(i2c1, ssd1306_i2c_clock * 1000);
-    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
-    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
-    gpio_pull_up(I2C_SDA);
-    gpio_pull_up(I2C_SCL);
-
-    // Processo de inicialização completo do OLED SSD1306
-    ssd1306_init();
-
-    // Preparar área de renderização para o display (ssd1306_width pixels por ssd1306_n_pages páginas)
-    struct render_area frame_area = {
-        start_column : 0,
-        end_column : ssd1306_width - 1,
-        start_page : 0,
-        end_page : ssd1306_n_pages - 1
-    };
-
-    calculate_render_area_buffer_length(&frame_area);
-
-    // zera o display inteiro
-    uint8_t ssd[ssd1306_buffer_length];
-    memset(ssd, 0, ssd1306_buffer_length);
-    render_on_display(ssd, &frame_area);
-
-restart:
-
-// Parte do código para exibir a mensagem no display (opcional: mudar ssd1306_height para 32 em ssd1306_i2c.h)
-// /**
-    char *text[] = {
-        "  Bem-vindos!   ",
-        "  Embarcatech   "};
-
-    int y = 0;
-    for (uint i = 0; i < count_of(text); i++)
-    {
-        ssd1306_draw_string(ssd, 5, y, text[i]);
-        y += 8;
-    }
-    render_on_display(ssd, &frame_area);*/
   // Inicializa entradas e saídas.
   stdio_init_all();
 
@@ -204,6 +159,11 @@ restart:
     if (gpio_get(BUTTON_A) && gpio_get(BUTTON_B)) {
       npClear();
       npWrite();
+    }
+    if (!gpio_get(BUTTON_A) && !gpio_get(BUTTON_B)) {
+      npClear();
+      npWrite();
+      sleep_ms(10);
     }
   }
 }
